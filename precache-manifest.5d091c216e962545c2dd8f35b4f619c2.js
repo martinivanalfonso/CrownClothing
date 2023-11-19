ba207092cc632dc8e476262767e0e9f9477e7563
Martin Alfonso <martin.alfonso@leverege.com>
2023-11-19 13:09:35 -0500

    Updates

---
+++ precache-manifest.5d091c216e962545c2dd8f35b4f619c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf4806487045790bfd7567133a61a9a",
+    "url": "/CrownClothing/index.html"
+  },
+  {
+    "revision": "af24286b4b534ba4d2cb",
+    "url": "/CrownClothing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "06ecfb04a0143bb48903",
+    "url": "/CrownClothing/static/js/2.74904558.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/CrownClothing/static/js/2.74904558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af24286b4b534ba4d2cb",
+    "url": "/CrownClothing/static/js/main.29be4132.chunk.js"
+  },
+  {
+    "revision": "b05e701f9f37eb7dec49",
+    "url": "/CrownClothing/static/js/runtime-main.691f289f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/CrownClothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/CrownClothing/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
